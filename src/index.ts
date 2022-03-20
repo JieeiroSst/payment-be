b34088ebc91cd57dee5eabda5306373814ac44e0
Alain Sst <luumanhquan.91@gmail.com>
2022-03-20 19:30:30 +0700

    update code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('Hello world!');
+import Koa from "koa";
+import Router from "koa-router";
+
+import Logger from "koa-logger";
+import Json from "koa-json";
+
+const app = new Koa();
+const router = new Router();
+
+router.get("/", async (ctx, next) => {
+    ctx.body = { msg: "hello world" };
+
+    await next();
+});
+
+app.use(Json());
+app.use(Logger());
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(3000, () => {
+    console.log("koa started");
+
+})
